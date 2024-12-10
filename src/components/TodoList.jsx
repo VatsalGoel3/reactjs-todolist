3fda03027f308164a6b1c87270a0655f15c17b8d
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2024-12-10 01:35:43 -0700

    Developed ToDoInput and ToDoList components: Added dynamic rendering of todos with mapping, integrated Font Awesome for icons, and created a ToDoCard component to encapsulate individual todo items.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import TodoCard from './TodoCard'
 
 export default function TodoList() {
+    let todos = [
+        'Brush your teeth',
+        'Eat healthy and go to gym',
+        'Prepare for exams and interview'
+    ]
+
   return (
-    <div>
-        TodoList
-    </div>
+    <ul className='main'>
+        {todos.map((todo, todoindex) => {
+            return (
+                <TodoCard key={todoindex}>
+                    <p>{todo}</p>
+                </TodoCard>
+            )
+        })}
+    </ul>
   )
 }
