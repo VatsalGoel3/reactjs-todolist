274849f34a7dd257d616321cc2010d3707caa50b
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2024-12-10 11:26:23 -0700

    Implemented state management for dynamic todos: Introduced React useState for managing todos, created a function to handle adding new todos, wired up input and button for interactive updates, and ensured real-time DOM updates using stateful variables.

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import TodoCard from './TodoCard'
 
-export default function TodoList() {
-    let todos = [
-        'Brush your teeth',
-        'Eat healthy and go to gym',
-        'Prepare for exams and interview'
-    ]
+export default function TodoList(props) {
+    const {todos} = props
 
   return (
     <ul className='main'>
