4e341e3259f7cb42f0b1378df40486e3dfd8815f
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2024-12-10 13:26:29 -0700

    Implemented edit functionality for todos: Added handleEditTodo function to populate the input field with the selected todo value for editing. Updated state management to allow shared access between components, enabling seamless editing and re-adding of todos. Wired up edit button logic in ToDoCard component.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import TodoList from "./components/TodoList"
 function App() {
 
   const [todos, setTodos] = useState([])
+  const [todoValue,setTodoValue] = useState('')
 
   function handleAddTodos(newTodo) {
     const newTodoList = [...todos, newTodo]
@@ -19,13 +20,19 @@ function App() {
   }
 
   function handleEditTodo(index) {
-
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
   }
 
   return (
     <>
-      <TodoInput todoValue={todoValue} handleAddTodos={handleAddTodos} />
-      <TodoList handleDeleteTodo={handleDeleteTodo} todos={todos} />
+      <TodoInput todoValue={todoValue} 
+      setTodoValue={setTodoValue} 
+      handleAddTodos={handleAddTodos} />
+      <TodoList handleDeleteTodo={handleDeleteTodo}
+      handleEditTodo={handleEditTodo} 
+      todos={todos} />
     </>
   )
 }
