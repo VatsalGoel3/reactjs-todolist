4a9c099e0eb1c131c7d1cd97064bd2c9bf97374e
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2025-02-05 21:58:37 -0700

    feat(auth): add login component

---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,42 @@
+// src/component/Auth/Login.jsx
+import React, {useRef, useState} from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import { useNavigate, Link } from "react-router-dom";
+
+export default function Login() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const { login } = useAuth();
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        try{
+            setError("");
+            setLoading(true);
+            await Login(emailRef.current.value, passwordRef.current.value);
+            navigate("/");
+        } catch {
+            setError("Failed to log in");
+        }
+        setLoading(false);
+    }
+
+    return (
+        <div>
+            <h2>Log In</h2>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <label>Email</label>
+                <input type="email" ref={emailRef} required />
+                <label>Password</label>
+                <input type="password" ref={passwordRef} required />
+                <button disabled={loading} type="submit">Log In</button>
+            </form>
+            <p>Need an account> <Link to="/signup">Sign Up</Link></p>
+        </div>
+    );
+}
