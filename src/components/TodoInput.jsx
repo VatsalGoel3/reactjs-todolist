9d2687213e5dae8fc3d8cb365302922ea18489c1
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2025-02-08 21:25:08 -0700

    feat(ui): refactor TodoInput to use MUI TextField and Button

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,16 +1,28 @@
+import React from "react";
+import { TextField, Button, Box } from "@mui/material";
 import { useState } from "react"
 
-export default function TodoInputprops(props) {
+export default function TodoInput(props) {
     const {handleAddTodos, todoValue, setTodoValue} = props
+
     return (
-        <header>
-            <input value={todoValue} onChange={(e) => {
-                setTodoValue(e.target.value)
-            }} placeholder="Enter todo..." />
-            <button onClick={() => {
-                handleAddTodos(todoValue)
-                setTodoValue('')
-            }}>Add</button>
-        </header>
-    )
+        <Box component="header" sx={{ display: "flex", gap: 2, p: 2 }}>
+            <TextField
+                value={todoValue}
+                onChange={(e) => setTodoValue(e.target.value)}
+                placeholder="Enter todo..."
+                variant="outlined"
+                fullWidth
+            />
+            <Button
+                variant="contained"
+                onClick={() => {
+                    handleAddTodos(todoValue);
+                    setTodoValue("");
+                }}
+            >
+                Add
+            </Button>
+        </Box>
+    );
 }
